fd83e67d3d0bdeab894be83d980edaf9e73504fb
jvcataquiz <jvcataquiz@gmail.com>
2022-05-21 12:03:20 +0800

    added temporary design for the gameplay and naming all button

--- TicTocToe-Vbnet/TicTocToe-Vbnet/Tictoctoe.Designer.vb
+++ TicTocToe-Vbnet/TicTocToe-Vbnet/Tictoctoe.Designer.vb
@@ -23,14 +23,122 @@ Partial Class Tictoctoe
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Tictoctoe))
+        Me.btnupperleft = New System.Windows.Forms.Button()
+        Me.btnupper = New System.Windows.Forms.Button()
+        Me.btnupperright = New System.Windows.Forms.Button()
+        Me.btnleft = New System.Windows.Forms.Button()
+        Me.btnmiddle = New System.Windows.Forms.Button()
+        Me.btnright = New System.Windows.Forms.Button()
+        Me.btnbottomleft = New System.Windows.Forms.Button()
+        Me.btnbottom = New System.Windows.Forms.Button()
+        Me.btnbottomright = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'btnupperleft
+        '
+        Me.btnupperleft.BackColor = System.Drawing.Color.Black
+        Me.btnupperleft.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnupperleft.Location = New System.Drawing.Point(12, 71)
+        Me.btnupperleft.Name = "btnupperleft"
+        Me.btnupperleft.Size = New System.Drawing.Size(100, 100)
+        Me.btnupperleft.TabIndex = 0
+        Me.btnupperleft.UseVisualStyleBackColor = False
+        '
+        'btnupper
+        '
+        Me.btnupper.BackColor = System.Drawing.Color.Black
+        Me.btnupper.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnupper.Location = New System.Drawing.Point(118, 71)
+        Me.btnupper.Name = "btnupper"
+        Me.btnupper.Size = New System.Drawing.Size(100, 100)
+        Me.btnupper.TabIndex = 1
+        Me.btnupper.UseVisualStyleBackColor = False
+        '
+        'btnupperright
+        '
+        Me.btnupperright.BackColor = System.Drawing.Color.Black
+        Me.btnupperright.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnupperright.Location = New System.Drawing.Point(224, 71)
+        Me.btnupperright.Name = "btnupperright"
+        Me.btnupperright.Size = New System.Drawing.Size(100, 100)
+        Me.btnupperright.TabIndex = 2
+        Me.btnupperright.UseVisualStyleBackColor = False
+        '
+        'btnleft
+        '
+        Me.btnleft.BackColor = System.Drawing.Color.Black
+        Me.btnleft.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnleft.Location = New System.Drawing.Point(12, 177)
+        Me.btnleft.Name = "btnleft"
+        Me.btnleft.Size = New System.Drawing.Size(100, 100)
+        Me.btnleft.TabIndex = 3
+        Me.btnleft.UseVisualStyleBackColor = False
+        '
+        'btnmiddle
+        '
+        Me.btnmiddle.BackColor = System.Drawing.Color.Black
+        Me.btnmiddle.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnmiddle.Location = New System.Drawing.Point(118, 177)
+        Me.btnmiddle.Name = "btnmiddle"
+        Me.btnmiddle.Size = New System.Drawing.Size(100, 100)
+        Me.btnmiddle.TabIndex = 4
+        Me.btnmiddle.UseVisualStyleBackColor = False
+        '
+        'btnright
+        '
+        Me.btnright.BackColor = System.Drawing.Color.Black
+        Me.btnright.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnright.Location = New System.Drawing.Point(224, 177)
+        Me.btnright.Name = "btnright"
+        Me.btnright.Size = New System.Drawing.Size(100, 100)
+        Me.btnright.TabIndex = 5
+        Me.btnright.UseVisualStyleBackColor = False
+        '
+        'btnbottomleft
+        '
+        Me.btnbottomleft.BackColor = System.Drawing.Color.Black
+        Me.btnbottomleft.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnbottomleft.Location = New System.Drawing.Point(12, 284)
+        Me.btnbottomleft.Name = "btnbottomleft"
+        Me.btnbottomleft.Size = New System.Drawing.Size(100, 100)
+        Me.btnbottomleft.TabIndex = 6
+        Me.btnbottomleft.UseVisualStyleBackColor = False
+        '
+        'btnbottom
+        '
+        Me.btnbottom.BackColor = System.Drawing.Color.Black
+        Me.btnbottom.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnbottom.Location = New System.Drawing.Point(118, 284)
+        Me.btnbottom.Name = "btnbottom"
+        Me.btnbottom.Size = New System.Drawing.Size(100, 100)
+        Me.btnbottom.TabIndex = 7
+        Me.btnbottom.UseVisualStyleBackColor = False
+        '
+        'btnbottomright
+        '
+        Me.btnbottomright.BackColor = System.Drawing.Color.Black
+        Me.btnbottomright.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnbottomright.Location = New System.Drawing.Point(224, 284)
+        Me.btnbottomright.Name = "btnbottomright"
+        Me.btnbottomright.Size = New System.Drawing.Size(100, 100)
+        Me.btnbottomright.TabIndex = 8
+        Me.btnbottomright.UseVisualStyleBackColor = False
         '
         'Tictoctoe
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(412, 291)
+        Me.ClientSize = New System.Drawing.Size(338, 436)
         Me.ControlBox = False
+        Me.Controls.Add(Me.btnbottomright)
+        Me.Controls.Add(Me.btnbottom)
+        Me.Controls.Add(Me.btnbottomleft)
+        Me.Controls.Add(Me.btnright)
+        Me.Controls.Add(Me.btnmiddle)
+        Me.Controls.Add(Me.btnleft)
+        Me.Controls.Add(Me.btnupperright)
+        Me.Controls.Add(Me.btnupper)
+        Me.Controls.Add(Me.btnupperleft)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.MaximizeBox = False
@@ -41,4 +149,13 @@ Partial Class Tictoctoe
 
     End Sub
 
+    Friend WithEvents btnupperleft As Button
+    Friend WithEvents btnupper As Button
+    Friend WithEvents btnupperright As Button
+    Friend WithEvents btnleft As Button
+    Friend WithEvents btnmiddle As Button
+    Friend WithEvents btnright As Button
+    Friend WithEvents btnbottomleft As Button
+    Friend WithEvents btnbottom As Button
+    Friend WithEvents btnbottomright As Button
 End Class
